f6c5921013c5c207fa9b986580f96888be7369d9
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2024-05-17 16:27:51 -0300

    Ex018

--- Mundo 1/aula008.py
+++ Mundo 1/aula008.py
@@ -1,2 +1,10 @@
 import math
 num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {}' .format(num, math.ceil(raiz))) #ceil arredonda pra cima; floor para baixo
+
+#from math import sqrt -> utilizar somente sqrt(raiz)
+
+import random
+num = random.randint(1,10)
+print(num)
