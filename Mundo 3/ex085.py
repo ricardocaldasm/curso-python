08e12d3d5b7cded5f4fa6ed626ccf6c68bc654cf
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2024-06-17 19:17:36 -0300

    ex086

---
+++ Mundo 3/ex085.py
@@ -0,0 +1,10 @@
+lista = [[], []]
+for i in range(1, 8):
+    valor = int(input("Digite o {}º valor: ".format(i)))
+    if valor % 2 == 0:
+        lista[0].append(valor)
+    else:
+        lista[1].append(valor)
+lista[0].sort()
+lista[1].sort()
+print(f'Os valores {lista[0]} são pares e o valores {lista[1]} são ímpares.')
