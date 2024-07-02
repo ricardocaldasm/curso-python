0aa7a4a541025225ca7179ac77916a7af269588f
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2024-07-02 13:17:14 -0300

    ex115

--- Mundo 3/ex115.py
+++ Mundo 3/ex115.py
@@ -8,13 +8,17 @@ if arqexiste(arq):
     print("Arquivo encontrado com sucesso.")
 else:
     print("Arquivo não existe.")
+    criararq(arq)
 
 while True:
     resposta = menu(["Ver Pessoas Cadastradas", "Cadastrar Pessoas", "Sair do Sistema"])
     if resposta == 1:
-        cabecalho("Opção 1")
+        lerarq(arq)
     elif resposta == 2:
-        cabecalho("Opção 2")
+        cabecalho("NOVO CADASTRO")
+        nome = str(input("Nome: "))
+        idade = leiaint("Idade: ")
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabecalho("Saindo do programa.")
         break
--- Mundo 3/ex115/lib/arquivo/__init__.py
+++ Mundo 3/ex115/lib/arquivo/__init__.py
@@ -1,7 +1,12 @@
+from ex115.lib.interface import *
+
+
 def arqexiste(arquivo):
     import os
 
-    os.chdir(r"C:\Users\riccm\OneDrive\Documentos\Cursos\Curso em Vídeo\curso-python\Mundo 3\ex115")
+    os.chdir(
+        r"C:\Users\riccm\OneDrive\Documentos\Cursos\Curso em Vídeo\curso-python\Mundo 3\ex115"
+    )
     try:
         abrir = open(arquivo, "rt")
         abrir.close()
@@ -9,3 +14,41 @@ def arqexiste(arquivo):
         return False
     else:
         return True
+
+
+def criararq(arquivo):
+    try:
+        abrir = open(arquivo, "+wt")
+        abrir.close()
+    except:
+        print("Houve um erro na criação do arquivo.")
+    else:
+        print(f"Arquivo {arquivo} criado com sucesso.")
+
+
+def lerarq(arquivo):
+    try:
+        abrir = open(arquivo, "rt")
+    except:
+        print("Erro ao ler o arquivo.")
+    else:
+        cabecalho("PESSOAS CADASTRADAS")
+        #print(abrir.read())  # readlines para mostrar em forma de lista
+        for linha in
+    finally:
+        abrir.close()
+
+
+def cadastrar(arquivo, nome="desconhecido", idade=0):
+    try:
+        abrir = open(arquivo, "at")
+    except:
+        print("Houve um erro na abertura do arquivo.")
+    else:
+        try:
+            abrir.write(f"{nome};{idade}\n")
+        except:
+            print("Houve um erro ao escrever os dados.")
+        else:
+            print(f"Novo registro de {nome} adicionado.")
+            abrir.close()
